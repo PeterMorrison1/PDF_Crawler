f276ac3f6195922115f17db31f3a74247a9b939b
PeterMorrison1 <morrisonman56@gmail.com>
2020-12-04 13:15:51 -0700

    Script complete

--- main.py
+++ main.py
@@ -3,38 +3,102 @@ import pprint
 from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
 import urllib.request
-from foreign_site import ForeignSite
+from urllib.parse import urlencode
+from openpyxl import Workbook
+from openpyxl import load_workbook
+from openpyxl.styles import Color, PatternFill, Font, Border
+from openpyxl.styles.colors import RED, GREEN, YELLOW, BLUE
+from openpyxl.cell import Cell
+
 
 pp = pprint.PrettyPrinter(indent=4)
+url_base = "https://pubmed.ncbi.nlm.nih.gov/?"
+
 
 def main():
     print("Start scraper")
+    path = input(
+        "Enter excel file name (ex: 'myfile.xlsx' <remove the quotes>): ")
 
-    # URL = "https://pubmed.ncbi.nlm.nih.gov/31285808/?from_single_result=Davari%2C+M.%2CGharibnaseri%2C+Z.%2CRavanbod%2C+R.%2CSadeghi%2C+A.+Health+status+and+quality+of+life+in+patients+with+severe+hemophilia+A%3A+A+cross-sectional+survey&expanded_search_query=Davari%2C+M.%2CGharibnaseri%2C+Z.%2CRavanbod%2C+R.%2CSadeghi%2C+A.+Health+status+and+quality+of+life+in+patients+with+severe+hemophilia+A%3A+A+cross-sectional+survey"
-    URL = "https://pubmed.ncbi.nlm.nih.gov/31136347/?from_single_result=Caruso+Brown%2C+A.+E.+Embracing+Discomfort+on+the+Path+to+Humility&expanded_search_query=Caruso+Brown%2C+A.+E.+Embracing+Discomfort+on+the+Path+to+Humility"
-    page = requests.get(URL)
+    wb = load_workbook(filename=path)
+    ws = wb['Sheet1']
 
-    soup = BeautifulSoup(page.content, 'html.parser')
+    for row in ws.rows:
+        try:
+            ref_id = row[0].value
+            author = row[1].value
+            title = row[2].value
+            year = row[3].value
+            search_dict = {'term': str(
+                author) + ' ' + str(title) + ' ' + str(year)}
+            URL = url_base + urlencode(search_dict)
+            print(URL)
 
-    id_list = soup.find(id='full-view-identifiers')
-    PMCID = id_list.find('span', class_='identifier pmc')
+            page = requests.get(URL)
 
-    if PMCID:
-        run_id_code(PMCID)
-    else:
-        link_div = soup.find('div', class_='full-text-links-list')
-        links = link_div.find_all('a', class_='link-item')
+            soup = BeautifulSoup(page.content, 'html.parser')
 
-        for link in links:
-            print("LINK: " + link.get("href").strip())
-            for_site = ForeignSite(link.get("href").strip(), '', '', '')
-            print(for_site.get_site_parser())
+            id_list = soup.find(id='full-view-identifiers')
+            PMCID = id_list.find('span', class_='identifier pmc')
+
+            if PMCID:
+                cell_selection = row[0]
+                is_downloaded = run_id_code(PMCID, ref_id, author.split(',')[0], year, cell_selection)
+                if is_downloaded:
+                    cell_selection.fill = PatternFill(
+                        fgColor=GREEN, fill_type='solid'
+                    )
+            else:
+                cell_selection = row[0]
+
+                cell_selection.fill = PatternFill(
+                    fgColor=RED, fill_type='solid')
+                
+                # ! This code can be used to collect the links to individual journals
+                # link_div = soup.find('div', class_='full-text-links-list')
+                # links = link_div.find_all('a', class_='link-item')
+
+                # download_url = None
+                # for link in links:
+                #     print("LINK: " + link.get("href").strip())
+                #     url = link.get("href").strip()
+                #     scraper = JournalScraper()
+                #     download_url = scraper.scrape_article(url)
+                #     if (download_url is not None):
+                #         download_from_url(download_url, ref_id, author, year)
+                #         break
+
+                # if download_url is not None:
+                #     cell_selection.fill = PatternFill(
+                #         fgColor=BLUE, fill_type='solid'
+                #     )
+                # else:
+                #     cell_selection.fill = PatternFill(
+                #         fgColor=RED, fill_type='solid'
+                #     )
+
+            print("\n")
+        except:
+            print("Error occurred on the row/webpage, coloured yellow")
+            cell_selection = row[0]
+            cell_selection.fill = PatternFill(
+                fgColor=YELLOW, fill_type='solid')
+
+    wb.save('Updated_Article_list.xlsx')
 
 
-        print("Probably need to run selenium now for each possible webpage...")
+# def download_from_url(pdf_url, ref_id, author, year):
+#     print("Download: " + pdf_url)
+#     # response = requests.get(pdf_url)
+#     with urllib.request.urlopen(pdf_url) as r:
+#         data = r.read()
+#         filename = str(ref_id) + '_' + str(author) + '_' + str(year)
+#         path = "/Users/Peter/Documents/PDF_Crawler/TestOutput/" + filename + ".pdf"
+#         with open(path, 'wb') as f:
+#             f.write(data)
 
 
-def run_id_code(PMCID):
+def run_id_code(PMCID, ref_id, author, year, cell_selection):
     newURL = "https://www.ncbi.nlm.nih.gov/pmc/utils/oa/oa.fcgi?id="
     id = PMCID.find('a', class_='id-link').text.strip()
     print("The id is: " + id)
@@ -45,19 +109,28 @@ def run_id_code(PMCID):
 
     root = ET.fromstring(new_page.content)
     # root = root.find("./records/record")
-    xml_links = root.findall("./records/record/link")
-    for link in xml_links:
-        type = link.get('format')
-        if type == "pdf":
-            pdf_url = link.get("href").strip()
-            print("Download: " + pdf_url)
-            # response = requests.get(pdf_url)
-            with urllib.request.urlopen(pdf_url) as r:
-                data = r.read()
-                file_name = "\\Users\\Peter\\Documents\\PDF_Crawler\\TestOutput\\" + id + ".pdf"
-                with open(file_name, 'wb') as f:
-                    f.write(data)
-                
+    if root.findtext("not Open Access"):
+        print("Article is not open access on PMC")
+        cell_selection.fill = PatternFill(
+            fgColor=YELLOW, fill_type='solid'
+        )
+        return False
+    else:
+        xml_links = root.findall("./records/record/link")
+        for link in xml_links:
+            type = link.get('format')
+            if type == "pdf":
+                pdf_url = link.get("href").strip()
+                print("Download: " + pdf_url)
+                # response = requests.get(pdf_url)
+                with urllib.request.urlopen(pdf_url) as r:
+                    data = r.read()
+                    filename = str(ref_id) + '_' + \
+                        str(author) + '_' + str(year)
+                    path = "/Users/Peter/Documents/PDF_Crawler/TestOutput/" + filename + ".pdf"
+                    with open(path, 'wb') as f:
+                        f.write(data)
+
 
 if __name__ == '__main__':
     main()
